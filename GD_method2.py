b8f0ddce0f13b83f27da79562dc7130778c305ae
Aftab Raza <raftab638@gmail.com>
2025-07-17 11:42:39 +0530

    Add files via upload

--- GD_method2.py
+++ GD_method2.py
@@ -1,15 +1,47 @@
+from sklearn.datasets import make_regression
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+
+X,y = make_regression(n_samples=100, n_features=1, n_informative=1, n_targets=1,noise=20,random_state=13)
+plt.scatter(X,y)
+
+# Add bias term to X
+X_b = np.c_[np.ones((X.shape[0], 1)), X]
+X_train, X_test, y_train, y_test = train_test_split(X_b, y, test_size=0.2, random_state=2)
+
+lr = LinearRegression()
+lr.fit(X_train,y_train)
+print(lr.coef_)
+print(lr.intercept_)
+
+y_pred = lr.predict(X_test)
+r2_score(y_test,y_pred)
+print(r2_score(y_test,y_pred))
+
 class GradientDescent:
-    def __init__(self, learning_rate, iterations,theta):
+    def __init__(self, learning_rate, iterations, theta):
         self.learning_rate = learning_rate
         self.iterations = iterations
         self.theta = theta
 
-    def fit(self,X,Y):
-        for i in range (self.iterations):
-            self.theta = self.theta - self.learning_rate * (X @ (Y-(X @ self.theta.T)))
-            #theta(t+1) = theta(t) - alpha[ sumation (i=0 to n)  X(i) * ( theta.T * X(i) - Y(i)]
-    def predict(self,X):
+    def fit(self, X, Y):
+        for i in range(self.iterations):
+            gradient = X.T @ (X @ self.theta - Y)
+            self.theta = self.theta - self.learning_rate * gradient
+        print("Trained theta:", self.theta)
+
+    def predict(self, X):
         return X @ self.theta.T
 
+initial_theta = np.zeros(X_train.shape[1])
+GD = GradientDescent(0.01,50,initial_theta)
 
-GD = GradientDescent(0.001,50,0)
+
+GD.fit(X_train,y_train)
+y_pred1 = GD.predict(X_test)
+r2_score(y_test,y_pred1)
+print(r2_score(y_test,y_pred1))
--- Gradient_descent.py
+++ Gradient_descent.py
@@ -1,29 +1,55 @@
+
+from sklearn.datasets import make_regression
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+
+X,y = make_regression(n_samples=100, n_features=1, n_informative=1, n_targets=1,noise=20,random_state=13)
+plt.scatter(X,y)
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=2)
+
+lr = LinearRegression()
+lr.fit(X_train,y_train)
+print(lr.coef_)
+print(lr.intercept_)
+
+y_pred = lr.predict(X_test)
+r2_score(y_test,y_pred)
+print(r2_score(y_test,y_pred))
+
 class GradientDescent:
-    def __init__(self,learning_rate,iterationn,M,B):
-        self.learning_rate = learning_rate
-        self.iterationn = iterationn
+    def __init__(self,learning_rate,iteration,M,B):
         self.M = M
         self.B = B
+        self.learning_rate = learning_rate
+        self.iteration = iteration
 
 
     def fit(self,X,Y):
         #calculatin B and M using GD
-        for i in range(self.iterationn):
+        for i in range(self.iteration):
             loss_slope_b = -2 * np.sum(Y - self.M*X.ravel() - self.B)
-            loss_slope_m= -2 * np.sum((Y - self.M*X.ravel() - self.B) * X.ravel())
+            loss_slope_m = -2 * np.sum((Y - self.M*X.ravel() - self.B) * X.ravel())
 
-            self.M = self.B - (self.learning_rate * loss_slope_b)
-            self.B = self.M - (self.learning_rate * loss_slope_m)
+            self.B = self.B - (self.learning_rate * loss_slope_b)
+            self.M = self.M - (self.learning_rate * loss_slope_m)
 
-            print(self.M, self.B)
+        print(self.M, self.B)
 
     def predict(self,X):
-        return self.M * X - self.B
+        return self.M * X + self.B
 
 #creating object of class GD
-GD = GradientDescent(0.001,50,100, 120)
+GD = GradientDescent(0.001,50,100, -120)
 
 #now we can use function fit by passing training dataset and function predict by passing test datasets
 
-
+GD.fit(X_train,y_train)
+y_pred = GD.predict(X_test)
+r2_score(y_test,y_pred)
+print(r2_score(y_test,y_pred))
 
